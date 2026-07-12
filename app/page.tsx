8245dd8cd638e753995fedd5eb76d6a98f5c7493
Syed Waqas Tayyab <waqastayyab2004@gmail.com>
2026-07-12 15:34:53 +0300

    Replace duplicate HireCTA at top with slim WelcomeBanner + move HireCTA to bottom

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,20 @@
+import WelcomeBanner from '@/components/home/WelcomeBanner'
 import Hero from '@/components/home/Hero'
+import HomeSummary from '@/components/home/HomeSummary'
 import TopicsPreview from '@/components/home/TopicsPreview'
 import ProjectsPreview from '@/components/projects/ProjectsPreview'
 import HireCTA from '@/components/home/HireCTA'
 import Newsletter from '@/components/home/Newsletter'
-import HomeSummary from '@/components/home/HomeSummary'
 
 export default function HomePage() {
   return (
     <>
-      <HireCTA />
+      <WelcomeBanner />
       <Hero />
       <HomeSummary />
       <TopicsPreview />
       <ProjectsPreview />
+      <HireCTA />
       <Newsletter />
     </>
   )
